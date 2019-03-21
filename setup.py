1c1562d94188d9af0ed34ccb22c80096f9694ea5
Trishul Nagenalli <tn74@duke.edu>
2019-03-21 17:46:34 -0400

    Committed Repo Changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='reem',
+    version='0.1',
+    packages=['reem'],
+    url='https://github.com/tn74/reem',
+    license='MIT',
+    author='Trishul Nagenalli',
+    author_email='tn74@duke.edu',
+    description='Redis Extendable Efficient Middleware'
+)
