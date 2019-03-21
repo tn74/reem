231b607b7aa84911c005f7dd3c055bf2900eca54
Trishul Nagenalli <tn74@duke.edu>
2019-03-21 18:41:19 -0400

    Please

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='reem',
     version='v0.0.1',
     packages=['reem'],
-    url='https://github.com/tn74/reem',
+    url='https://www.github.com/tn74/reem',
     license='MIT',
     author='Trishul Nagenalli',
     author_email='tn74@duke.edu',
