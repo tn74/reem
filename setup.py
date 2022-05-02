715079cd2b35b98e6359e5414e7e4389ba4b1085
Kris Hauser <kkhauser@illinois.edu>
2022-05-02 13:54:57 -0400

    Added improved readme reading

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
 # read the contents of your README file
-import codecs
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with codecs.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='reem',
-    version='0.1.0',
+    version='0.1.1',
     packages=['reem'],
     url='https://www.github.com/krishauser/reem',
     license='Apache 2.0',
@@ -23,5 +22,5 @@ setup(
         'numpy',
       ],
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
 )
